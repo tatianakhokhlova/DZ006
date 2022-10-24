00c31be87abb859ec7e42abbb0b36a6b5469e422
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-24 22:36:19 +0300

    Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы

---
+++ 001/Program.cs
@@ -0,0 +1,18 @@
+﻿void CrossPoint(int k1, int b1, int k2, int b2)
+{
+int x = (b1-b2)/(k2-k1);
+int y = (k2*b1-k1*b2)/(k2-k1);
+if(k1==k2) Console.Write("Заданные прямые не пересекаются!");
+else
+Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+}
+
+Console.WriteLine("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите значение k1: ");
+int k1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine() ?? "0");
+CrossPoint(k1,b1,k2,b2);
