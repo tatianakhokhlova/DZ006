d8fa020cf9cb75c9a7a07b5e001338431e476f78
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-26 14:52:53 +0300

    Написать программу масштабирования фигуры

---
+++ 003/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу масштабирования фигуры
+
+System.Globalization.NumberFormatInfo numberInfo = System.Globalization.NumberFormatInfo.InvariantInfo;
+Console.WriteLine("Введите координаты:");
+string inputLine = Console.ReadLine();
+Console.WriteLine("Введите множитель:");
+double inputMult = double.Parse(Console.ReadLine(), numberInfo);
+Console.WriteLine(ChangeSize(inputLine, inputMult));
+
+
+string ChangeSize(string input, double multiple)
+{
+    string result = "";
+    string[] points = input.Replace("(", "").Replace(")", "").Split(" ");
+    foreach (string point in points)
+    {
+        string x = $"{double.Parse(point.Split(",")[0], numberInfo) * multiple}".Replace(",", ".");
+        string y = $"{double.Parse(point.Split(",")[1], numberInfo) * multiple}".Replace(",", ".");
+        result += $"({x},{y}) ";
+    }
+    return result;
+}
