61d14eb73a1b8c86a3f9f7e7be29a7373a5a8898
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-26 14:41:20 +0300

    точки пересечения прямых

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,18 +1,27 @@
-﻿void CrossPoint(int k1, int b1, int k2, int b2)
+﻿//Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы.
+
+Console.WriteLine("Введите k1: ");
+int k1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите b1: ");
+int b1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите b2: ");
+int b2 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите k2: ");
+int k2 = int.Parse(Console.ReadLine() ?? "0");
+
+if ((k1 == k2) && (b1 == b2))
 {
-int x = (b1-b2)/(k2-k1);
-int y = (k2*b1-k1*b2)/(k2-k1);
-if(k1==k2) Console.Write("Заданные прямые не пересекаются!");
-else
-Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+
 }
 
-Console.WriteLine("Введите значение b1: ");
-int b1 = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите значение k1: ");
-int k1 = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите значение b2: ");
-int b2 = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите значение k2: ");
-int k2 = int.Parse(Console.ReadLine() ?? "0");
-CrossPoint(k1,b1,k2,b2);
+else
+{
+int x = (b2 - b1) / (k1 - k2);
+int y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+Console.WriteLine($"точка пересечения двух прямых ({x}, {y})");
+}
